f50e59a987ab7f15584cab922ec52b4d777ec534
Javi <falces@gmail.com>
2020-02-12 19:40:30 +0100

    Errores

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogController extends Controller
 {
 	public function articleDetail($_locale, $_year, $_slug, $_format)
 	{
+		// Escupir error
+		// throw new \Exception('Algo ha ido mal');
+
+		// Error 404
+		// throw new NotFoundHttpException('El artículo no existe');
+
 		return $this->render('twig/article/article_detail.'.$_format.'.twig', array(
 			'language' => $_locale,
 			'year' => $_year,
