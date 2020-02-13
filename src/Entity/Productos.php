940cf62f866f9338bb4cfcfd727336983a6cc1a6
Javi <falces@gmail.com>
2020-02-13 20:11:03 +0100

    Entidad productos

---
+++ src/Entity/Productos.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductosRepository")
+ */
+class Productos
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prize;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrize(): ?int
+    {
+        return $this->prize;
+    }
+
+    public function setPrize(int $prize): self
+    {
+        $this->prize = $prize;
+
+        return $this;
+    }
+}
